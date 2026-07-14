52faf6ae8db96ea1b8d0c7320b1bbe0b9dcc8676
Navdeep2810s <navdeepgumber007@gmail.com>
2026-07-14 17:31:24 +0530

    Connect frontend to Render backend

--- fakevsrealnews/src/pages/Contact.jsx
+++ fakevsrealnews/src/pages/Contact.jsx
@@ -1,13 +1,65 @@
 import Navbar from "../components/navbar/navbar";
+import "./Contact.css";
 
-function Home() {
+function Contact() {
   return (
     <>
       <Navbar />
 
-      <h1>Home Page</h1>
+      <section className="contact">
+
+        <h1>Contact Us</h1>
+
+        <p className="contact-text">
+          Have any questions or suggestions? We'd love to hear from you.
+        </p>
+
+        <div className="contact-container">
+
+          <div className="contact-info">
+
+            <h2>Get in Touch</h2>
+
+            <p><strong>Email:</strong> support@fakenewsai.com</p>
+
+            <p><strong>Phone:</strong> +91 98xxx xxxxx</p>
+
+            <p>
+              <strong>Address:</strong><br />
+              ABC....<br />
+              ..., India
+            </p>
+
+          </div>
+
+          <form className="contact-form">
+
+            <input
+              type="text"
+              placeholder="Your Name"
+            />
+
+            <input
+              type="email"
+              placeholder="Your Email"
+            />
+
+            <textarea
+              rows="6"
+              placeholder="Your Message"
+            ></textarea>
+
+            <button type="submit">
+              Send Message
+            </button>
+
+          </form>
+
+        </div>
+
+      </section>
     </>
   );
 }
 
-export default Home;
+export default Contact;
